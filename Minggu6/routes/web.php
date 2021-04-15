2948e8336286cfbee84a9f9793e50afaef9d9dea
Ilham Taufiqul Khaliq <ilhamtaufiqul75@gmail.com>
2021-04-15 17:28:27 +0700

    Tugas minggu 6

---
+++ Minggu6/routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//minggu 3
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/belajar', function () {
+    echo '<h1>Hello World</h1>';
+    echo '<p>Sedang Belajar Laravel</p>';
+});
+Route::get('page/{nomor}', function ($nomor) {
+    return 'Ini Halaman Ke-' . $nomor;
+});
+Route::get('/gambar', function () {
+    return view('gambar');
+});
+
+//minggu 4
+Route::get('/user', 'DetailProfileController@index');
+Route::resource('user', 'DetailProfileController');
+Route::get("/home", function(){
+    return view("homepage");
+});
+
+//minggu 5
+// frontend
+Route::group(['namespace' => "Frontend"], function()
+{
+    Route::resource('home', 'HomeController');
+});
+
+// backend
+Route::group(['namespace' => "Backend"], function()
+{
+    Route::resource('dashboard', 'DashboardController');
+});
